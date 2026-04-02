58a04880d8986c4ef92a1a44e6d5fb1f7f0cfd92
PaulMFleming <paul.stillbreathing@gmail.com>
2026-04-02 17:47:13 +0100

    First pass of simple calculator

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -2,6 +2,23 @@ class SimpleCalculator
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
 
   def self.calculate(first_operand, second_operand, operation)
-    raise 'Please implement the SimpleCalculator.calculate method'
+    raise(ArgumentError) unless first_operand.is_a?(Integer) && second_operand.is_a?(Integer)
+    raise(UnsupportedOperation) unless ALLOWED_OPERATIONS.include?(operation)
+    if operation == '+'
+      answer = first_operand + second_operand
+    elsif operation == '/'
+      if second_operand == 0
+        return 'Division by zero is not allowed.'
+      else
+        answer = first_operand / second_operand
+      end
+    elsif operation == '*'
+      answer = first_operand * second_operand
+    end
+
+    "#{first_operand} #{operation} #{second_operand} = #{answer}"
   end
+end
+
+class UnsupportedOperation < StandardError
 end
---
+++ ruby/simple-calculator/simple_calculator_spec.rb
@@ -0,0 +1,41 @@
+require_relative 'simple_calculator'
+
+RSpec.describe SimpleCalculator do
+  describe 'calculate' do
+    it 'correctly works with addition' do
+      expect(described_class.calculate(22, 25, '+')).to eq('22 + 25 = 47')
+    end
+
+    it 'correctly works with multiplication' do
+      expect(described_class.calculate(3, 21, '*')).to eq('3 * 21 = 63')
+    end
+
+    it 'correctly works with division' do
+      expect(described_class.calculate(72, 9, '/')).to eq('72 / 9 = 8')
+    end
+
+    it 'raises an exception when dividing by zero' do
+      expect(described_class.calculate(33, 0, '/')).to eq('Division by zero is not allowed.')
+    end
+
+    it 'raises ArgumentError if first operand is not a number' do
+      expect{ described_class.calculate('1', 2, '+') }.to raise_error(ArgumentError)
+    end
+
+    it 'raises ArguementError if second operand is not a number' do
+      expect{ described_class.calculate(1, '2', '+') }.to raise_error(ArgumentError)
+    end
+
+    it 'raises UnsupportedOperation for non valid operations' do
+      expect{ described_class.calculate(1, 2, '**') }.to raise_error(UnsupportedOperation)
+    end
+
+    it 'raises UnsupportedOperation for non valid operations' do
+      expect{ described_class.calculate(1, 2, nil) }.to raise_error(UnsupportedOperation)
+    end
+
+    it 'raises UnsupportedOperation for non valid operations' do
+      expect{ described_class.calculate(1, 2, '') }.to raise_error(UnsupportedOperation)
+    end
+  end
+end
